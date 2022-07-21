e8f57b3401f75fa3703358f9ec83c4aeb6b1726d
Lira Artem <artemlira84@gmail.com>
2022-07-21 22:57:23 +0300

    добавил картинки, описал стили и добавил описание в разметку

---
+++ src/ShowImg.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ShowImg = ({ image, className }) => (
+   <div className="className">
+      <img src={image} alt="className" />
+   </div>
+);
+
+export default ShowImg;
